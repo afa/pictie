4ccaa538c60e589123eed0ce3c1ae2f82b643ed8
afa <afa@afanote>
2015-01-29 01:16:55 +0300

    fix for url decoding

--- models/pict.rb
+++ models/pict.rb
@@ -8,14 +8,12 @@ class Pict < Sequel::Model
     pict = where(url: url).first
     unless pict
       key = Digest::MD5.hexdigest(CONFIG['key'])
-      p key
       bf = Crypt::Blowfish.new(key)
 
-      uri = bf.decrypt_string(Base64.urlsafe_decode64(url))
-      p uri
+      uri = bf.decrypt_string(Base64.urlsafe_decode64(url)).to_s.strip
       res = Net::HTTP.get_response(URI(uri))
       if res.is_a?(Net::HTTPSuccess)
-        p res['content-type']
+        # p res['content-type']
         pict = create(url: url)
         FileUtils.mkdir_p(File.dirname(pict.path))
         File.open(pict.path, 'w'){|f| f.write(res.body) }
